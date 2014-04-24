05e3be5e3151bd5bb797426a95041231cf83438d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-04-24 20:21:27 +0100

    Fix weld injection, make it work alongside Selenium

--- src/test/java/nicebank/TestSteps.java
+++ src/test/java/nicebank/TestSteps.java
@@ -10,13 +10,10 @@ public class TestSteps {
     @Inject
     Funky funky;   // This never gets injected
 
-/*  This gives me the error "cucumber.runtime.CucumberException: class nicebank.TestSteps doesn't have an empty constructor. If you need DI, put cucumber-picocontainer on the classpath
-"
     @Inject
     public TestSteps(Funky f){
         funky = f;
     }
-*/
 
     @Given("^I am very funky$")
     public void i_am_very_funky() throws Throwable {
